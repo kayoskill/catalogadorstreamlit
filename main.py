747d2fc83fb4a7f3de504260f768baf37a23bf0c
Kayo <kayo@obti.net.br>
2021-12-24 01:26:50 -0300

    update

---
+++ main.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from modulos.conexao import Conexao_Iq
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title='Sr. Diretor', page_icon='〽')
+
+
+@st.cache
+def velas_frame(dicionario):
+    frame = pd.DataFrame.from_dict(dicionario, orient='index')
+    frame.drop(columns=['id', 'at'], inplace=True)
+    frame.rename(columns={'from': 'inicio', 'to': 'fim',
+                          'open': 'abertura', 'close': 'fechamento'}, inplace=True)
+    frame['i_index'] = frame['inicio']
+    frame['i_index'] = pd.to_datetime(
+        frame['i_index'] - 10800, unit='s')
+    frame['inicio'] = pd.to_datetime(
+        frame['inicio']-10800, unit='s').dt.time
+    frame['fim'] = pd.to_datetime(
+        frame['fim'] - 10800, unit='s')
+    frame.set_index('i_index', inplace=True)
+    frame['direcao'] = np.where(
+        frame['abertura'] == frame['fechamento'], 'doji', np.where(frame['abertura'] > frame['fechamento'], 'call', 'put'))
+    return frame
+
+
+@st.cache
+def catalogar(frame):
+    lista_horario = frame['inicio'].unique()
+    dicionario = {}
+    for l in lista_horario:
+        call = frame.loc[(frame['inicio'] == l) & (
+            frame['direcao'] == 'call')].shape[0]
+        put = frame.loc[(frame['inicio'] == l) & (
+            frame['direcao'] == 'put')].shape[0]
+        doji = frame.loc[(frame['inicio'] == l) & (
+            frame['direcao'] == 'doji')].shape[0]
+        soma = (call+put+doji)
+        p_call, p_put, p_doji = (
+            call / soma) * 100, (put / soma) * 100, (doji / soma) * 100
+        dicionario.update(
+            {l: {'call': round(p_call, 2), 'put': round(p_put, 2), 'doji': round(p_doji, 2)}})
+        df = pd.DataFrame.from_dict(dicionario, orient='index')
+    return df
+
+
+st.sidebar.title('Painel de Configurações')
+st.sidebar.subheader('Acesso a conta IQ:')
+usuario = st.sidebar.text_input('Digite seu usuario:')
+senha = st.sidebar.text_input('Digite sua senha:', type='password')
+conectou, ativos, timeframe, periodo = False, False, False, False
+if usuario and senha:
+    iq = Conexao_Iq(usuario, senha)
+    conectou = iq.checando()
+    if not conectou:
+        st.sidebar.error(conectou)
+if conectou:
+    st.sidebar.subheader('Ajustes para informações:')
+    ativos = st.sidebar.multiselect('Selecione os ativos:', iq.listar_ativos())
+    periodo = st.sidebar.number_input(
+        'Selecione o periodo:', min_value=1, max_value=60)
+    timeframe = st.sidebar.selectbox('Selecione Timeframe:', [
+        '1', '5', '15', '30', '60', '120'])
+    porcentagem = st.sidebar.number_input(
+        'Igual ou maior:', min_value=1.00, max_value=100.00)
+
+# Centro
+st.title('Catalogador e Check-List')
+
+if ativos and timeframe and periodo:
+    for ativo in ativos:
+        dic_velas = iq.velas(ativo, timeframe, periodo)
+        fra_velas = velas_frame(dic_velas)
+        cata = catalogar(fra_velas)
+        print(cata)
+        st.table(cata)
---
+++ modulos/conexao.py
@@ -0,0 +1,86 @@
+from iqoptionapi.stable_api import IQ_Option
+import streamlit as st
+import time
+import pandas as pd
+import numpy as np
+
+
+class Conexao_Iq():
+    def __init__(self, email, senha) -> None:
+        self._api = IQ_Option(email, senha)
+
+    def checando(self):
+        self.conectou, self._rea = self._api.connect()
+        if self.conectou:
+            print('Conectado')
+            return 'Conectado'
+        else:
+            if self._rea == "[Errno -2] Name or service not known":
+                print('Sem Conexão')
+                return 'Sem Conexão'
+            elif self._rea == 'error_password':
+                print('Erro Senha')
+                return 'Error Senha'
+
+    def listar_ativos(self):
+        lista = []
+        dicionario = self._api.get_all_open_time()
+        for k, v in dicionario['digital'].items():
+            lista.append(k)
+        return lista
+
+    def velas(self, ativo, timeframe, periodo):
+        periodo = ((60 * 24) / int(timeframe)) * int(periodo)
+        timeframe = int(timeframe) * 60
+        dados = self._api.get_candles(
+            ativo, timeframe, int(periodo), time.time())
+        dicionario = {}
+        for dado in dados:
+            dicionario.update({dado['from']: dado})
+        return dicionario
+
+    @staticmethod
+    def velas_frame(dicionario):
+        frame = pd.DataFrame.from_dict(dicionario, orient='index')
+        frame.drop(columns=['id', 'at'], inplace=True)
+        frame.rename(columns={'from': 'inicio', 'to': 'fim',
+                              'open': 'abertura', 'close': 'fechamento'}, inplace=True)
+        frame['i_index'] = frame['inicio']
+        frame['i_index'] = pd.to_datetime(
+            frame['i_index'] - 10800, unit='s')
+        frame['inicio'] = pd.to_datetime(
+            frame['inicio']-10800, unit='s').dt.time
+        frame['fim'] = pd.to_datetime(
+            frame['fim'] - 10800, unit='s')
+        frame.set_index('i_index', inplace=True)
+        frame['direcao'] = np.where(
+            frame['abertura'] == frame['fechamento'], 'doji', np.where(frame['abertura'] > frame['fechamento'], 'call', 'put'))
+        return frame
+
+    @staticmethod
+    def catalogar(frame):
+        lista_horario = frame['inicio'].unique()
+        dicionario = {}
+        for l in lista_horario:
+            call = frame.loc[(frame['inicio'] == l) & (
+                frame['direcao'] == 'call')].shape[0]
+            put = frame.loc[(frame['inicio'] == l) & (
+                frame['direcao'] == 'put')].shape[0]
+            doji = frame.loc[(frame['inicio'] == l) & (
+                frame['direcao'] == 'doji')].shape[0]
+            soma = (call+put+doji)
+            p_call, p_put, p_doji = (
+                call / soma) * 100, (put / soma) * 100, (doji / soma) * 100
+            dicionario.update(
+                {l: {'call': round(p_call, 2), 'put': round(p_put, 2), 'doji': round(p_doji, 2)}})
+            df = pd.DataFrame.from_dict(dicionario, orient='index')
+        return df
+
+
+if __name__ == '__main__':
+    api = Conexao_Iq('kcfservicos95@gmail.com', '90576edkf')
+    if api.checando() == 'Conectado':
+        dados = api.velas('EURUSD', 5, 10)
+        f = api.velas_frame(dados)
+        d = api.catalogar(f)
+        print(d)
